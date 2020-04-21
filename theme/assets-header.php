5d397a39b403210a82f34ec9b3646964d4a666e6
Leslie Nielsen <nis.websites@gmail.com>
2020-04-21 07:45:00 -0700

    Added content to assets header

--- theme/assets-header.php
+++ theme/assets-header.php
@@ -1,2 +1,19 @@
-<?php
-//put contents here
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<!-- Meta Data -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Lite Frame Website Framework</title>
+        <meta name="description" content="Light weight framework for creating websites quickly"/>
+        <meta name="keywords" content="HTML5, Bootstrap 3, CSS3, javascript, PHP, onepage, responsive, mobile"/>
+        <meta name="author" content="Leslie K. Nielsen"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+       	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    	
+        <!-- Web Fonts -->        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">		
+		
+	</head>
